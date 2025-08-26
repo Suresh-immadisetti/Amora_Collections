528dfe7dca5d522d2b15eceecdd94ccabddc8df1
suresh <suresh.i@rjaytechnologies.com>
2025-08-26 21:08:47 +0530

    Configured vite for GitHub Pages deployment with some modification

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Amora_Collections/', // 👈 very important for GitHub Pages
   plugins: [react()],
+  base: '/Amora_Collections/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
