093ec2b3db5e598ea3d26d8ec0c551500cbd492f
suresh <suresh.i@rjaytechnologies.com>
2025-08-26 21:13:23 +0530

    Configured vite for 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Amora_Collections/',
+  base: '/Amora_Collections/', // 👈 required for GitHub Pages
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react'], // keep this if you had issues with lucide-react
   },
-});
+})
