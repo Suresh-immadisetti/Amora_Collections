b54770eba1c318b960efc7acef02031d42ce228d
suresh <suresh.i@rjaytechnologies.com>
2025-08-27 11:51:15 +0530

    Add / in vite file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Amora_Collections/', // 👈 required for GitHub Pages
+  base: '/', // 👈 required for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'], // keep this if you had issues with lucide-react
   },
